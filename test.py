696f11474638f59b0f4b55fa5c4537c8a882d53a
Yebitch <yarik.selin@bk.ru>
2024-09-29 19:15:35 +0800

    some mvp

--- test.py
+++ test.py
@@ -4,10 +4,13 @@ import calendar
 from db import DB_connector
 from dateutil.relativedelta import relativedelta
 
-today = datetime.now().date()
-
-day = ((today + timedelta(days=62)).replace(day=today.day))
-
-while(today <= day):
-    print(today)
-    today += timedelta(days=1)
+import re 
+# x^2-10x+4
+m = [4, -10, 1]
+dx_dm = []
+counter = 1
+for i in m[1:]:
+    dx_dm.append(i*counter)
+    counter += 1
+    
+print(dx_dm)
